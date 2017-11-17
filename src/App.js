e1df1b050fe60bddf3cee23560ec3c603503e7c9
Konsta Purtsi <kovipu@utu.fi>
2017-11-17 16:39:56 +0200

    fetching the sightings now works

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sightings: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(this.props.server + '/sightings')
+      .then(res => {
+        console.log(res)
+        const sightings = res.data;
+        this.setState({ sightings });
+      });
+  }
+
   render() {
-    var sightings = [];
-    fetch('http://localhost:8081/sightings')
-    .then(res => res.json())
-    .then((out) => { sightings.push(out) }).catch(err => console.error(err));
-
-    console.log(typeof(sightings))
-    console.log('ASDASDSAD ', sightings)
-
-    const sightings_ = sightings.foreach(s => { console.log(s)})
-    // const sightings_ = sightings.map((s, i) => {
-    //   return <li>{s}</li>
-    // });
-    console.log(typeof(sightings_))
-    console.log(sightings_)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <ul>{sightings_}</ul>
+        {this.props.server}
+        <ul>
+          {this.state.sightings.map(sighting => 
+            <li key={sighting.id}>{sighting.species} ---- {sighting.description}</li>
+          )}
+        </ul>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App server='http://localhost:8081'/>, document.getElementById('root'));
 registerServiceWorker();
