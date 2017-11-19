506565babdfa2899c9feff5f37e1fc9225068046
Konsta Purtsi <kovipu@utu.fi>
2017-11-19 11:45:15 +0200

    finishing touches to SightingList

--- src/components/SightingList.js
+++ src/components/SightingList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 export default class SightingList extends Component {
@@ -15,31 +14,29 @@ export default class SightingList extends Component {
 
   render() {
     return (
-      <div>
-        <BootstrapTable ref='sightingTable' data={this.props.sightings}>
-          <TableHeaderColumn dataField='id' hidden isKey>
-            ID
-          </TableHeaderColumn>
-          <TableHeaderColumn 
-            dataField='dateTime' 
-            width='200' 
-            dataSort 
-            sortFunc={this.sortByDate} 
-            dataFormat={(t) => new Date(t).toLocaleString()}
-          >
-            Time
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField='species' width='100'>
-            Species
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField='count' width='80' dataAlign='right'>
-            Count
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField='description'>
-            Description
-          </TableHeaderColumn>
-        </BootstrapTable>
-      </div>
+      <BootstrapTable ref='sightingTable' data={this.props.sightings}>
+        <TableHeaderColumn dataField='id' hidden isKey> {/* a key field is required */}
+          ID
+        </TableHeaderColumn>
+        <TableHeaderColumn 
+          dataField='dateTime' 
+          width='200' 
+          dataSort 
+          sortFunc={this.sortByDate} 
+          dataFormat={(t) => new Date(t).toLocaleString() /* make the ISO dates human readable */}
+        >
+          Time
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField='species' width='100'>
+          Species
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField='count' width='80' dataAlign='right'>
+          Count
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField='description'>
+          Description
+        </TableHeaderColumn>
+      </BootstrapTable>
     )
   }
 }
