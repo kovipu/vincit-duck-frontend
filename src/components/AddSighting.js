aa2cc8f35ae5807d9fdc1f50b50bdd04f3f0978e
Konsta Purtsi <kovipu@utu.fi>
2017-11-26 16:07:12 +0200

    capitalized species names in dropdown

--- src/components/AddSighting.js
+++ src/components/AddSighting.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
-import { InputGroup, InputGroupButton, Input, Button } from 'reactstrap';
 import Moment from 'moment';
 import momentLocalizer from 'react-widgets-moment';
 import simpleNumberLocalizer from 'react-widgets-simple-number';
 import { DateTimePicker, DropdownList, NumberPicker } from 'react-widgets';
+import { InputGroup, InputGroupButton, Input } from 'reactstrap';
 
 import './AddSighting.css';
 import 'react-widgets/dist/css/react-widgets.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-Moment.locale('en');
+Moment.locale('fi');
 momentLocalizer();
 simpleNumberLocalizer();
 
 export default class AddSighting extends Component {
+  formatSpecies(s) {
+    if (s === undefined) { return null }
+
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
   render() {
+    const speciesOptions = this.props.species.map(
+      s => s.name)
+
     return (
       <div className='form'>
-        <DateTimePicker dropUp step={1} />
+        <DateTimePicker 
+          dropUp 
+          step={ 1 } 
+          format='DD/MM/YYYY, HH:mm' />
         <DropdownList 
           dropUp 
-          data={this.props.species} 
-          textField='name'
-          defaultValue={this.props.species[0]} />
-        <NumberPicker min={0} max={999} />
+          data={speciesOptions} 
+          defaultValue={speciesOptions[0]}
+          textField={s => this.formatSpecies(s)}
+          />
+        <NumberPicker 
+          min={ 0 } 
+          max={ 999 } />
         <InputGroup>
           <Input />
           <InputGroupButton color='secondary'>Save</InputGroupButton>
